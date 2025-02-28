510b2f3c0007ae391fcfa829ab0ae0eb811e30b5
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2025-02-28 22:06:21 +0100

    Refactor loader.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,10 @@
 Changelog
 ---------
 
+0.0.2
+-----
+
+1) Refactor loader.
 
 0.0.1
 ~~~~~~
---
+++ config/loader.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'file_phar' => 'recipe/common.php',
+    ],
+    [
+        'package' => 'sourcebroker/deployer-instance',
+    ],
+    [
+        'package' => 'sourcebroker/deployer-extended-media',
+    ],
+    [
+        'package' => 'sourcebroker/deployer-typo3-media',
+    ],
+];
